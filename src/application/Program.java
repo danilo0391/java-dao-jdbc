c7402e08cf107c3466be77ae4a0ba118fceeb0fe
Danilo Pereira <danilo_tw@hotmail.com>
2023-07-16 10:24:48 +0100

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,5 +40,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n==== TEST 5: seller update ====");
+        seller = sellerDao.findById(2);
+        seller.setName("Marta Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
     }
 }
