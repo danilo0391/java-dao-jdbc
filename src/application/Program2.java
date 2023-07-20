758469cb02cbf6469ba77f574ca39948c8864a39
Danilo Pereira <danilo_tw@hotmail.com>
2023-07-20 20:43:29 +0100

    Implementation Department FindById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,10 +2,15 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.entities.Department;
 import model.impl.DepartmentDaoJDBC;
 
 public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("==== TEST 1: department findById ====");
+        Department dp = departmentDao.findById(6);
+        System.out.println(dp);
     }
 }
